01e1201746995612175382cd60aebb555ffdb23c
Amex shipper <cruise.Amexshipper.com>
2021-02-05 02:52:10 -0500

    all

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 dbPassword3 = 'mongodb://localhost:27017/mern';
-dbPassword = 'mongodb+srv://cryptonite:cryptonite@cluster0.6gjtg.mongodb.net/<dbname>?retryWrites=true&w=majority'
+dbPassword = 'mongodb+srv://coinworld:coinworld@cluster0.0expj.mongodb.net/<dbname>?retryWrites=true&w=majority'
 
 module.exports = {
     mongoURI: dbPassword
